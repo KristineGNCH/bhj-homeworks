13161689ca34fa2dc43b3c751c52ebdd96821fcf
Kristine Goncharenko <kristine.gnch@gmail.com>
2023-09-17 11:45:14 +0300

    updated

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,25 +1,31 @@
 
-let deadCounter = 0;
-let lostCounter = 0;
+let deadCounter = document.getElementById('dead');
+let lostCounter = document.getElementById('lost');
+
 getHole = index => document.getElementById(`hole${index}`);
 
 
-for (holeIndex = 1; holeIndex < getHoleCount(); holeIndex++) {
-    getHole(holeIndex).addEventListener('click', function () {
-        if (getHole(holeIndex).classList.contains('hole_has-mole')) {
+for (holeIndex = 1; holeIndex < 10; holeIndex++) {
+    let hole = getHole(holeIndex);
+    hole.addEventListener('click', function () {
+        if (hole.classList.contains('hole_has-mole')) {
             deadCounter++;
+            deadCounter.textContent = deadCounter;
         } else {
             lostCounter++;
             lostCounter.textContent = lostCounter;
         }
         if (deadCounter === 10) {
             alert('Вы победили!');
+            deadCounter = 0;
+            lostCounter = 0;
             deadCounter.textContent = lostCounter;
             lostCounter.textContent = lostCounter;
         }
-
         if (deadCounter === 5) {
             alert('Вы проиграли!');
+            deadCounter = 0;
+            lostCounter = 0;
             deadCounter.textContent = lostCounter;
             lostCounter.textContent = lostCounter;
         }
