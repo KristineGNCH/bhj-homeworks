94d15e4360097c25866e1ba360be349e5af7143f
Kristine Goncharenko <kristine.gnch@gmail.com>
2023-10-11 14:19:22 +0300

    updated

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,2 +1,29 @@
 'use strict'
 
+const taskInput = document.getElementById('task__input');
+const tasksList = document.getElementById('tasks__list');
+const form = document.querySelector('.tasks__control');
+
+form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    if (taskInput.value.length > 0) {
+        tasksList.insertAdjacentHTML('beforeend',
+            `<div class="task">
+    <div class="task__title">${taskInput.value}</div>
+    <a href="#" class="task__remove">&times;</a></div>`
+        );
+        taskInput.value = '';
+    };
+})
+
+
+tasksList.addEventListener('click', (e) => {
+    const remove = e.target.matches('.task__remove');
+    const task = e.target.closest('.task');
+
+    if (remove) {
+        task.remove();
+        e.preventDefault();
+    }
+})
